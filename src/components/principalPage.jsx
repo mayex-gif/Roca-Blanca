64010a8a404aa1c8a45c568feb4f1a144553dc1e
mayex-gif <alcaldealejo53@gmail.com>
2024-08-07 18:05:08 -0300

    Pagina principal 07-08

---
+++ src/components/principalPage.jsx
@@ -0,0 +1,45 @@
+import '../css/principalPage.css'
+
+import ropaImg from '../images/ropa.jpg'
+import calzadosImg from '../images/calzados.jpg'
+
+import { Link } from 'react-router-dom';
+
+import Slide from '../components/slide';
+
+const PrincipalPage = () => {
+    return (
+        <>
+            <Slide />
+
+            <div id='spacerY' className=''>
+                <div className='row m-0 p-0 d-flex justify-content-center'>
+                    <div className="card p-0 m-5 col-11 col-md-5" id='card1'>
+                        <img src={calzadosImg} className="card-img p-0 m-0" id='card-img' alt="..." />
+                        <div className="card-img-overlay d-flex align-items-end justify-content-center">
+                            <div>
+                                <h5 className="card-title text-white text-center fs-4"><strong>CALZADOS</strong></h5>
+                                <p className="card-text text-white text-center">Calzados aptos para mujeres y hombres.</p><div className='text-center'>
+                                    <Link to={'/'} className="btn btn-outline-light text-center" id='card-btn'><strong>IR AHORA</strong></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                    <div className="card p-0 m-5 col-11 col-md-5" id='card1'>
+                        <img src={ropaImg} className="card-img-top p-0 m-0" id='card-img' alt="..." />
+                        <div className="card-img-overlay d-flex align-items-end justify-content-center">
+                            <div>
+                                <h5 className="card-title text-white text-center fs-4"><strong>VESTIMENTA</strong></h5>
+                                <p className="card-text text-white text-center">Vestimenta apta para mujeres y hombres.</p><div className='text-center'>
+                                    <Link to={'/'} className="btn btn-outline-light text-center" id='card-btn'><strong>IR AHORA</strong></Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PrincipalPage;
